2ad9a15f23800de9e8ca122fc4eb832f3f8063a2
Emin <eminyilmzz@gmail.com>
2024-02-13 23:24:25 +0300

    fix: bad default value for int radius

--- case-be/src/main/java/com/eminyilmazz/casebe/controller/LocationController.java
+++ case-be/src/main/java/com/eminyilmazz/casebe/controller/LocationController.java
@@ -19,7 +19,7 @@ public class LocationController {
 
     @CrossOrigin(origins = "*") // Allow requests from any origin
     @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<PlaceDTO> get(@RequestParam(name = "long") double longitude, @RequestParam(name = "lat") double latitude, @RequestParam(name = "rad", required = false, defaultValue = "1000.0") int rad) {
+    public List<PlaceDTO> get(@RequestParam(name = "long") double longitude, @RequestParam(name = "lat") double latitude, @RequestParam(name = "rad", required = false, defaultValue = "1000") int rad) {
 
         return locationService.get(LocationDTO.builder()
                 .longitude(longitude)
